bf975b40e6c2b3bf64350f43903b668c5ba12b3e
Liam matulick <liammatulick@gmail.com>
2022-05-21 18:16:40 +1000

    Update1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import './style.css';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+import { HomeScreen } from './screens/home';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBwW8H1NiKnG8k4x3VZo-PJIp-GSMlI1Ck",
+    authDomain: "tswsecuritysolutions.firebaseapp.com",
+    databaseURL: "https://tswsecuritysolutions-default-rtdb.firebaseio.com",
+    projectId: "tswsecuritysolutions",
+    storageBucket: "tswsecuritysolutions.appspot.com",
+    messagingSenderId: "617742575499",
+    appId: "1:617742575499:web:3fc155638b4d8b81b5b18e",
+    measurementId: "G-H43811WCZ5"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <HomeScreen />
   );
 }
