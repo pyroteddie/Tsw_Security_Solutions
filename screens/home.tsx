9e28b529d312a955b224e63a2d1212809a2edf99
pyroteddie <liammatulick@gmail.com>
2022-07-25 09:24:08 +1000

    2 files modified, 1 added

--- screens/home.tsx
+++ screens/home.tsx
@@ -7,7 +7,7 @@ export default function HomeScreen() {
       <div>
         <section>
         <h1>TSW Security Solutions - Your North Queensland Security Experts</h1>
-        <p>Since 2016, TSW Security Solutions has been providing exceptional security services in the Cairns and surrounding region.  Family owned and operated, TWS is well equipped to assist with your business or event security needs, and we welcome your enquiry.</p>
+        <p className='Sec1_blub'>Since 2016, TSW Security Solutions has been providing exceptional security services in the Cairns and surrounding region.  Family owned and operated, TWS is well equipped to assist with your business or event security needs, and we welcome your enquiry.</p>
         </section>
         <section className='ServicesSection'>
         <a className='ServiceItem_1'>
@@ -23,7 +23,23 @@ export default function HomeScreen() {
         </section>
         <section>
         <h1>Industry Sectors</h1>
-        <p>Start editing to see some magic happen </p>
+        <div className='IndContainer'>
+          <div className='IndItem'>
+            <div className='IndItemTitle_Left'>Commercial</div>
+            <div className='IndItemText'>We cater to a number of commerical facilitys </div>
+          </div>
+
+          <div className='IndItem'>
+            <div className='IndItemText'>We cater to a number of commerical facilitys </div>
+            <div className='IndItemTitle_Right'>Government</div>
+          </div>
+
+          <div className='IndItem'>
+            <div className='IndItemTitle_Left'>Residential</div>
+            <div className='IndItemText'>We cater to a number of commerical facilitys </div>
+          </div>
+        </div>
+        <p></p>
         </section>
         <section className='Footer'> 
         
