8e23b19ade8d38e775fb45d129bf5f746e867a29
pyroteddie <liammatulick@gmail.com>
2022-08-31 01:27:53 +1000

    1 file modified

--- App.tsx
+++ App.tsx
@@ -9,22 +9,7 @@ import Header from './components/header'
 import ServicesScreen from './screens/services'
 import AboutScreen from './screens/about'
 import Footer from './components/footer'
-  const firebaseConfig = {
-    apiKey: "AIzaSyBwW8H1NiKnG8k4x3VZo-PJIp-GSMlI1Ck",
-    authDomain: "tswsecuritysolutions.firebaseapp.com",
-    databaseURL: "https://tswsecuritysolutions-default-rtdb.firebaseio.com",
-    projectId: "tswsecuritysolutions",
-    storageBucket: "tswsecuritysolutions.appspot.com",
-    messagingSenderId: "617742575499",
-    appId: "1:617742575499:web:3fc155638b4d8b81b5b18e",
-    measurementId: "G-H43811WCZ5"
-  };
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
-
+  
 export default function App() {
   return (
     <Router>
